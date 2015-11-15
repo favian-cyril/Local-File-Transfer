3cc45bea66411c5f6849ff9f28daad54948c3318
favial-cyril <favian.cyril@gmail.com>
2015-11-15 19:13:47 +0700

    update

--- Desktop/Client/client.py
+++ Desktop/Client/client.py
@@ -1,5 +1,6 @@
 import socket
 import md5sum
+import time
 
 TCP_IP = 'localhost'
 TCP_PORT = 9001
@@ -9,39 +10,44 @@ FILE_PATH = 'Data\\'
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((TCP_IP, TCP_PORT))
 def createMD5SUM():
-    x = md5sum.grab_files('.')
+    x = md5sum.grab_files('Data\\')
     lista = []
     for i in x:
-        y = md5sum.md5(i[1])
-    lista.append((i[0],y))
+        y = md5sum.md5(i)
+        lista.append((i,y))
     md5sum.createFile(lista)
     
-def sendMD5SUM(md5):
-    sock.send('MD5SUM_FILE'encode('ascii'))
-    file = open(md5)
+def sendMD5SUM():
+    sock.send('MD5SUM_COMPARE'.encode('ascii'))
+    file = open('MD5SUM.txt', 'rb')
+    time.sleep(0.5)
     while True:
         data = file.read(BUFFER_SIZE)
         while data:
-            sock.send(data)
+            sock.sendall(data)
             data = file.read(BUFFER_SIZE)
         if not data:
+            sock.send('END_FILE_TRANSFER'.encode('ascii'))
             file.close()
             break
     
-def recieveFile(name):
+def recFile(name):
     with open(FILE_PATH+name, 'wb') as f:
         print('file opened')
         while True:
             #print('receiving data...')
             data = sock.recv(BUFFER_SIZE)
             print('data=%s', (data))
-            if not data:
+            if data.decode('ascii') == 'END_FILE_TRANSFER':
                 f.close()
                 print('file close()')
                 break
             # write data to a file
             f.write(data)
 def closeConnection():
-    print('Successfully get the file')
     sock.close()
     print('connection closed')
+
+
+sendMD5SUM()
+recFile('Text.txt')
