40632deb58019de36e4f6cc825f6df42a18c61df
favial-cyril <favian.cyril@gmail.com>
2015-11-22 20:55:56 +0700

    CompareLocalFile

--- Desktop/Client/md5sum.py
+++ Desktop/Client/md5sum.py
@@ -40,9 +40,54 @@ def createFile(arrPath,name='MD5SUM.txt'):
     for name, filedir in arrPath:
         print(name, filedir, file=filetext)
     print("File MD5SUM Created")
+
+def compareLocalMD5(path = None, checksum1 = 'MD5SUM.txt'):
+    """
+    Compares local file with latest MD5SUM. Input must be in the form of string
+    ex: .\thisFolder
+    """
+    # Creates the md5 of local content
+    files_in_dir = grab_files(path)
+    local_list = [] # List of filenames and its md5 value
+    for i in files_in_dir:
+        y = md5(i)
+        local_list.append((i,y))
+    local_dict = dict(local_list) # Change it to a dictionary
+
+    # Open the previously created MD5SUM
+    fileMD = open(checksum1, "r").read()
+    listMD = fileMD.split("\n")
+    temp =[]
+    for i in listMD:
+        temp.append(i[:-33])
+        temp.append(i[-32:])
+    listMD = temp
+    dictMD = dict(zip(*[iter(listMD)]*2))
+    try:
+        dictMD.pop("",None)
+    except:
+        pass
+    
+    
+    # Determine which files are missing/created anew
+    result = []
+    for key in dictMD:
+        if key in local_dict:
+            if dictMD[key] == local_dict[key]: 
+                result.append((key, "MATCH"))
+            elif dictMD[key] != local_dict[key]:
+                result.append((key, "UPDATE"))
+        if key not in local_dict:
+            result.append((key, "DELETE"))
+    for key in local_dict:
+        if key not in dictMD:
+            result.append((key, "UPLOAD"))
+    return result
+    
+    
 def compareFileDifference(checksum1 = None, checksum2 = None):
     """
-    TODO: Compare actual file difference and
+    Compare actual file difference and
     return a list with status of file
     """
     # Open Files
@@ -51,8 +96,22 @@ def compareFileDifference(checksum1 = None, checksum2 = None):
     # Split 'em and put 'em as dicks
     listMD = fileMD.split("\n")
     listMDOutput = fileMDOutput.split("\n")
+
+    temp =[]
+    for i in listMD:
+        temp.append(i[:-33])
+        temp.append(i[-32:])
+    listMD = temp
+    temp =[]
+    for i in listMDOutput:
+        temp.append(i[:-33])
+        temp.append(i[-32:])
+    listMDOutput = temp
+
     dictMD = dict(zip(*[iter(listMD)]*2))
     dictMDOutput = dict(zip(*[iter(listMDOutput)]*2))
+    
+    
     # Determine which have the same key and same md5 value
     result = []
     for key in dictMD:
@@ -69,11 +128,12 @@ def compareFileDifference(checksum1 = None, checksum2 = None):
     return result
 
 
-##x = grab_files('Data//')
-##lista = []
-##for i in x:
-##    y = md5(i)
-##    lista.append((i,y))
-##createFile(lista)
+##    x = md5sum.grab_files(FILE_PATH)
+##    lista = []
+##    for i in x:
+##        y = md5sum.md5(i)
+##        lista.append((i,y))
+##    md5sum.createFile(lista)
+
 
     
