e75f42f8a89447b103395bf7b2fae3aab0f74a19
Velta Azizah Destiana <velta.azizah@gmail.com>
2015-11-23 00:02:27 +0700

    Basic GUI (non connected)

--- Desktop/Client/GUI/gui.py
+++ Desktop/Client/GUI/gui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter.filedialog import askdirectory
 
 class Window(Tk):
     def __init__(self, parent):
@@ -9,6 +10,10 @@ class Window(Tk):
     def initialize(self):
         # Connect window
         self.geometry("300x150+30+30")
+        self.label = Label(self, text='Please fill in below to access the application :')
+        self.label.pack()
+
+        # Continue to application main page
         self.wButton = Button(self, text='Connect', command = self.OnButtonClick)
         self.wButton.pack()
 
@@ -19,14 +24,35 @@ class Window(Tk):
         self.top.geometry("600x400+30+30")
         self.top.transient(self)
         self.wButton.config(state='disabled')
-        self.topButton = Button(self.top, text="CLOSE", command = self.OnChildClose)
-        self.topButton.pack()
+        
+        self.top.label = Label(self.top, text='You have connected. Have fun!')
+        self.top.label.pack()
+        
+        # SyncKong's logo
+        self.top.image = PhotoImage(file='gorilla.gif')
+        Label(self.top, image=self.top.image).pack()
+
+        self.refresh = Button(self.top, text="Sync with Kong!", command=self.refresh)
+        self.refresh.pack()
+
+        self.path = Button(self.top, text="Set Path", command=self.path)
+        self.path.pack()
+
+        self.backButton = Button(self.top, text="Back To Connect", command = self.OnChildClose)
+        self.backButton.pack()
 
     def OnChildClose(self):
         # Close and go back to 'Connect' state
         self.wButton.config(state='normal')
         self.top.destroy()
 
+    def refresh(self):
+        pass
+
+    def path(self):
+        # Get folder's directory
+        self.filepath = askdirectory()
+    
 if __name__ == "__main__":
     window = Window(None)
     window.title("Connect To Sync Kong")
