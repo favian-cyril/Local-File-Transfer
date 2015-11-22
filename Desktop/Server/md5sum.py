af7ff780d132ed8c6405d3e3bf73e5b13968ae51
favial-cyril <favian.cyril@gmail.com>
2015-11-22 19:15:14 +0700

    Old

--- Desktop/Server/md5sum.py
+++ Desktop/Server/md5sum.py
@@ -69,11 +69,11 @@ def compareFileDifference(checksum1 = None, checksum2 = None):
     return result
 
 
-##x = grab_files('.')
-##lista = []
-##for i in x:
-##    y = md5(i[1])
-##    lista.append((i[0],y))
-##createFile(lista)
+##    x = md5sum.grab_files(FILE_PATH)
+##    lista = []
+##    for i in x:
+##        y = md5sum.md5(i)
+##        lista.append((i,y))
+##    md5sum.createFile(lista)
 
     
