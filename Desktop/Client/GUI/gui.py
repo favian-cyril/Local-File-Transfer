7ea7daea5b5d56dcf04aeb25bfb48fbb97a331cc
Velta Azizah Destiana <velta.azizah@gmail.com>
2015-11-23 00:23:28 +0700

    Basic GUI finished

--- Desktop/Client/GUI/gui.py
+++ Desktop/Client/GUI/gui.py
@@ -9,9 +9,17 @@ class Window(Tk):
 
     def initialize(self):
         # Connect window
-        self.geometry("300x150+30+30")
-        self.label = Label(self, text='Please fill in below to access the application :')
+        self.geometry("500x250+30+30")
+        self.label = Label(self, text='Please fill in below to access the application! \n')
         self.label.pack()
+        
+        Label(self, text="IP Address").pack()
+        entry1 = Entry(self)
+        entry1.pack()
+
+        Label(self, text="Host").pack()
+        entry2 = Entry(self)
+        entry2.pack()
 
         # Continue to application main page
         self.wButton = Button(self, text='Connect', command = self.OnButtonClick)
@@ -32,12 +40,15 @@ class Window(Tk):
         self.top.image = PhotoImage(file='gorilla.gif')
         Label(self.top, image=self.top.image).pack()
 
+        # Sync Button
         self.refresh = Button(self.top, text="Sync with Kong!", command=self.refresh)
         self.refresh.pack()
-
+        
+        # Set Path Directory Button
         self.path = Button(self.top, text="Set Path", command=self.path)
         self.path.pack()
-
+        
+        # Back To Connect Button
         self.backButton = Button(self.top, text="Back To Connect", command = self.OnChildClose)
         self.backButton.pack()
 
